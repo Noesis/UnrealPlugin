60e3e265f65f07c59292019e6dbc7cc494a269f7
hcpizzi <hcpizzi@hotmail.com>
2017-12-09 18:01:49 +0100

    BuildNoesisGUIPlugin.bat: Copy Intermediate directory and supress overwrite prompt

--- BuildNoesisGUIPlugin.bat
+++ BuildNoesisGUIPlugin.bat
@@ -7,5 +7,6 @@ set "uniqueFileName=%tmp%\NoesisGuiPlugin%RANDOM%"
 if exist "%uniqueFileName%" goto :uniqLoop
 
 "%~dp0..\..\Binaries\DotNET\AutomationTool.exe" BuildPlugin -Plugin="%~dp0NoesisGUI.uplugin" -TargetPlatforms=Win64 -Package="%uniqueFileName%"
-xcopy "%uniqueFileName%\Binaries\*" "%~dp0Binaries" /e /i
+xcopy "%uniqueFileName%\Binaries\*" "%~dp0Binaries" /e /i /y
+xcopy "%uniqueFileName%\Intermediate\*" "%~dp0Intermediate" /e /i /y
 rmdir /s /q "%uniqueFileName%
