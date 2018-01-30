841194264575135064d66b46e29a5858520a0f7c
hcpizzi <hcpizzi@hotmail.com>
2018-01-30 19:14:23 +0100

    Missed NoesisSDK.h because of .gitignore

---
+++ Source/Noesis/NoesisSDK.h
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// NoesisGUI - http://www.noesisengine.com
+// Copyright (c) 2013 Noesis Technologies S.L. All Rights Reserved.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#ifdef PLATFORM_WINDOWS
+#if PLATFORM_WINDOWS
+#include "AllowWindowsPlatformTypes.h"
+#endif
+#endif
+
+#define EA_CHAR32_NATIVE 0
+#include "Noesis_pch.h"
+
+#ifdef PLATFORM_WINDOWS
+#if PLATFORM_WINDOWS
+#include "HideWindowsPlatformTypes.h"
+#endif
+#endif
