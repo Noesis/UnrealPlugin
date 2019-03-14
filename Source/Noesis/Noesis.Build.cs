48cf28622e5c5220c24ea0740307ac97ae53de07
unknown <hcpizzi@hotmail.com>
2019-03-14 23:54:27 +0100

    Plugin updated to Noesis SDK 2.2.0

--- Source/Noesis/Noesis.Build.cs
+++ Source/Noesis/Noesis.Build.cs
@@ -23,8 +23,8 @@ public class Noesis : ModuleRules
 		PublicIncludePaths.Add(NoesisInteractivityIncludePath);
 
 		// Let's try to make sure the right version of the SDK is in the right place.
-		const string RequiredRevision = "(r7939)";
-		const string RequiredVersionName = "2.2.0rc2";
+		const string RequiredRevision = "(r7948)";
+		const string RequiredVersionName = "2.2.0";
 
 		PublicDefinitions.Add("NOESIS_VERSION_NAME=\"" + RequiredVersionName + "\"");
 		if (!Directory.Exists(NoesisBasePath))
